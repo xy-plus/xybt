5b3f5d060995668c9799639a76877cf53f9e79cf
luffy <511568816@qq.com>
2022-12-01 14:40:25 +0800

    source

---
+++ hello_asm/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+    // while (*envp)
+    //     printf("%s\n", *envp++);
+    // while (*argv)
+    //     printf("%s\n", *argv++);
+    // printf("%s\n", *argv++);
+    // printf("%s\n", *argv++);
+    printf("%s\n", *argv++);
+}
